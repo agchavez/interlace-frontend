70272b6db221d2d3a8e94f5e0d5628bd38ec4fae
agchavez <angelchavez.325.ac@gmail.com>
2023-09-01 06:11:32 -0600

    feat: :sparkles: Home actualizado

--- src/modules/home/pages/HomePage.tsx
+++ src/modules/home/pages/HomePage.tsx
@@ -1,12 +1,5 @@
 
-import { useNavigate } from "react-router-dom";
-import { Button } from "@mui/material";
-
 export default function HomePage() {
-    const navigate = useNavigate();
     return <>
-        <Button onClick={() => navigate("/user")}>Ir a usuarios</Button>
-        <Button onClick={() => navigate("/user/register")}>registro</Button>
-
     </>
 }
