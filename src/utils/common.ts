6190edf5079f8aeb6428b2e83484fbaaed9d8581
Gerson Castillo <gersoncastilloarce@gmail.com>
2023-09-12 21:57:48 -0600

    agregando qr a la impresion, falta informacion sobre de donde sacar los datos

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,1 +1,6 @@
 export const regextEmail = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+export const getM = (m: number) => {
+    const mon = ["ENE", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AGO", "SEP", "OCT", "NOV", "DIC"]
+    const res = mon[m]
+    return res || ""
+}
