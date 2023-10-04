321c352ca0a072ef5649aea8b7996a77ba810ba9
Gerson Castillo <gersoncastilloarce@gmail.com>
2023-10-04 16:48:42 -0600

    feature: :sparkles: agregado filtro de tipo de tracking a gestion

--- src/interfaces/tracking.ts
+++ src/interfaces/tracking.ts
@@ -137,6 +137,7 @@ export interface TrackerQueryParams extends BaseQueryParams {
   status?: 'COMPLETE' | 'PENDING' | 'EDITED';
   filter_date?: FilterDate;
   onlyMyTreckers?: boolean;
+  type?: "IMPORT" | "LOCAL"
 }
 
 export interface TrackerProductDetailQueryParams extends BaseQueryParams {
